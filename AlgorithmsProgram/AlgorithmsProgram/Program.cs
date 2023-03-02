454c0746f1ca1a44923c6064f20e7bd6c379a932
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-02 21:22:43 +0530

    Add UC1-InsertionSort

---
+++ AlgorithmsProgram/AlgorithmsProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select The Option");
+            Console.WriteLine("1.Insertion Sort");
+            Console.WriteLine();
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    InsertionSort insertion = new InsertionSort();
+                    insertion.DisplayInsertionSort();
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
